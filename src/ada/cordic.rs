1024b4831a1259f997773a57c176fb035f87a8f9
Zora <38050295+PPiinngg@users.noreply.github.com>
2023-05-19 00:38:57 +0100

    moving to laptop, new cordic translation half done

--- src/ada/cordic.rs
+++ src/ada/cordic.rs
@@ -1,8 +1,8 @@
 // use lazy_static::lazy_static;
 // use std::ops::Neg;
 
-// const ACCURACY: usize = 11;
-// const SIGN_MASK: u32 = 2_147_483_648;
+// const ACCURACY: usize = 8;
+// const SIGN_MASK: u32 = 0x80000000u32;
 
 // // currently a pretty much direct translation of matlab impl
 // // https://en.wikipedia.org/wiki/CORDIC#Implementation
@@ -43,9 +43,48 @@
 //     cordic_rotate(radians, &mut cos, &mut sin);
 //     sin
 // }
+use lazy_static::lazy_static;
+use std::ops::Neg;
+
+//////////////////////////////////////
+
+const ACCURACY: usize = 8;
+
+//////////////////////////////////////
+
+const SIGN_MASK: u32 = 0x80000000u32;
+const F32_EXPONENT_MASK: u32 = 0x7f800000u32;
+const F32_NOT_EXPONENT_MASK: u32 = !F32_EXPONENT_MASK;
+
+lazy_static! {
+	static ref K: f32 = (0..ACCURACY)
+		.map(|i| { 1f32 / (1f32 + (-2f32 * (i as f32)).exp2()).sqrt() })
+		.product();
+	static ref ANGLES: Vec<f32> = (0..ACCURACY)
+		.map(|i| { ((-(i as f32)).exp2()).atan() })
+		.collect();
+}
+
+pub fn cordic_rotate(radians: &f32, cos: &mut f32, sin: &mut f32) {
+	let mut cos_out: f32 = *cos;
+	let mut sin_out: f32 = *sin;
+	let mut delta: f32 = *radians;
+
+	for i in 0..ACCURACY {
+		let delta_sign: u32 = delta.to_bits() & SIGN_MASK;
+		let rotator: f32 = f32::from_bits((i as f32).neg().exp2().to_bits() ^ delta_sign);
+		
+		let cos_uint: u32 = cos_out.to_bits();
+		let sin_uint: u32 = sin_out.to_bits();
+		cos_out += f32::from_bits(cos_prev); // LINE 53 IN C++ IMPL
+		sin_out -= f32::from_bits(sin_prev); // LINE 52 IN C++ IMPL
+
+		delta -= ANGLES[i];
+	}
+}
 
 pub fn temp_rotate(radians: &f32, cos: &mut f32, sin: &mut f32) {
-    let in_angle = sin.atan2(*cos);
-    *cos = (in_angle + radians).cos();
-    *sin = (in_angle + radians).sin();
+	let in_angle = sin.atan2(*cos);
+	*cos = (in_angle + radians).cos();
+	*sin = (in_angle + radians).sin();
 }
